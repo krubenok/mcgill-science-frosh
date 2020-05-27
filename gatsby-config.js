c5b1301d7ef066df70a259e2a154dda434d78153
Kyle Rubenok <kyle@rubenok.ca>
2020-05-27 15:07:32 -0700

    Initial commit from gatsby: (https://github.com/LekoArts/gatsby-starter-portfolio-cara.git)

---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
+module.exports = {
+  siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // Of course you can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
+    siteTitleAlt: `Science Frosh 2020`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      // See the theme's README for all available options
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Science Frosh 2020`,
+        short_name: `Science Frosh 2020`,
+        description: `An unforgettable Science Frosh 2020 is coming soon.`,
+        start_url: `/`,
+        background_color: `#141821`,
+        theme_color: `#f6ad55`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+  ],
+}
---
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Parallax } from "react-spring/renderprops-addons.cjs"
+import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout"
+import Hero from "@lekoarts/gatsby-theme-cara/src/components/hero"
+import Contact from "@lekoarts/gatsby-theme-cara/src/components/contact"
+
+const Cara = () => (
+    <Layout>
+        <Parallax pages={2}>
+            <Hero offset={0} factor={1} />
+            <Contact offset={1} factor={1} />
+        </Parallax>
+    </Layout>
+)
+
+export default Cara
