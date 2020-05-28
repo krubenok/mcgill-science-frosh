2d0c4cbc89249e042f11550a292ea33d00d49a27
Kyle Rubenok <kyle@rubenok.ca>
2020-05-27 19:51:23 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,22 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Science Frosh 2020`,
+    // Used for the title template on pages other than the index site
+    siteTitle: `Science Frosh 2020`,
+    // Default title of the page
+    siteTitleAlt: `SUS Frosh`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `An unforgettable Science Frosh 2020 is coming soon.`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `https://susfrosh.com`,
+    // Used for SEO
+    siteDescription: `An unforgettable Science Frosh 2020 is coming soon.`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `@krubenok`,
   },
   plugins: [
     {
