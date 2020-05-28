4571de1519a4585f6d73be5e5a0bd9a7990990db
Kyle Rubenok <kyle@rubenok.ca>
2020-05-27 20:12:10 -0700

    Teaks for mobile

--- src/@lekoarts/gatsby-theme-cara/components/contact.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/contact.tsx
@@ -37,7 +37,7 @@ const Contact = ({
                     sx={{
                         position: `relative`,
                         height: `full`,
-                        svg: { width: `100%`, height: `40vh` },
+                        svg: { width: `100%`, height: `70vh` },
                     }}
                 >
                     <svg
