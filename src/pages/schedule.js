39a1d881802df53d39c47169a9145a86e0b8e2f6
Kyle Rubenok <kyle@rubenok.ca>
2020-08-14 10:53:26 -0700

    add schedule

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,21 +1,36 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import Layout from '../components/layout/Layout'
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import Layout from '../components/layout/Layout';
+import Img from 'gatsby-image';
+import { StaticQuery, graphql } from 'gatsby';
 
 export default () => (
-  <Layout>
-    <div className='application'>
-      <Helmet>
-        <html lang='en' amp />
-        <meta charSet='utf-8' />
-        <title>Froshters, Inc. | Schedule</title>
-        <link rel='canonical' href='https://susfrosh.com/schedule' />
-      </Helmet>
-    </div>
-    <section className='container mx-auto px-4 pt-8 mb-20 space-y-4 text-center max-w-screen-lg mx-auto'>
-      <h1 className='text-4xl lg:text-5xl xl:text-6xl font-bold'>
-        Stay tuned for the 2020 schedule and information on all events
-      </h1>
-    </section>
-  </Layout>
-)
+  <StaticQuery
+    query={graphql`
+      query {
+        schedule: file(relativePath: { eq: "schedule.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Layout>
+        <div className="application">
+          <Helmet>
+            <html lang="en" amp />
+            <meta charSet="utf-8" />
+            <title>Froshters, Inc. | Schedule</title>
+            <link rel="canonical" href="https://susfrosh.com/schedule" />
+          </Helmet>
+        </div>
+        <section className="container mx-auto px-4 pt-8 mb-20 space-y-4 text-center max-w-screen-lg mx-auto">
+          <Img fluid={data.schedule.childImageSharp.fluid} alt="Frosh Schedule" />
+        </section>
+      </Layout>
+    )}
+  />
+);
