85cdf61541bac744c5bb85a69be33f881ed1723c
Kyle Rubenok <kyle@rubenok.ca>
2020-08-14 10:50:41 -0700

    add sponsors

--- src/pages/partners.js
+++ src/pages/partners.js
@@ -1,14 +1,77 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
-import Layout from '../components/layout/Layout'
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import Layout from '../components/layout/Layout';
 
 export default () => (
   <StaticQuery
     query={graphql`
       query {
-        frosh4: file(relativePath: { eq: "frosh-4.jpg" }) {
+        alto: file(relativePath: { eq: "sponsors/alto.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        coc: file(relativePath: { eq: "sponsors/coc.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        diperie: file(relativePath: { eq: "sponsors/diperie.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        freshii: file(relativePath: { eq: "sponsors/freshii.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        hatter: file(relativePath: { eq: "sponsors/hatter.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        licm: file(relativePath: { eq: "sponsors/licm.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        mcgillpizza: file(relativePath: { eq: "sponsors/mcgillpizza.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        meddirection: file(relativePath: { eq: "sponsors/meddirection.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        pannizza: file(relativePath: { eq: "sponsors/pannizza.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        wize: file(relativePath: { eq: "sponsors/wize.png" }) {
           childImageSharp {
             fluid(maxWidth: 1000) {
               ...GatsbyImageSharpFluid
@@ -19,27 +82,53 @@ export default () => (
     `}
     render={data => (
       <Layout>
-        <div className='application'>
+        <div className="application">
           <Helmet>
-            <html lang='en' amp />
-            <meta charSet='utf-8' />
+            <html lang="en" amp />
+            <meta charSet="utf-8" />
             <title>Froshters, Inc. | Partners</title>
-            <link rel='canonical' href='https://susfrosh.com/partners' />
+            <link rel="canonical" href="https://susfrosh.com/partners" />
           </Helmet>
         </div>
-        <section className='container mx-auto px-4 pt-8 mb-20 space-y-4'>
-          <h1 className='text-4xl lg:text-5xl xl:text-6xl font-bold'>Partners</h1>
-          <Img
-            fluid={data.frosh4.childImageSharp.fluid}
-            alt='Photo of DJ at a concert with large crowd'
-          />
-          <h2 className='text-xl lg:text-2xl xl:text-3xl'>
-            If you and your business are interested in sponsoring Science Frosh, please contact us
-            at <a href='mailto:frosh@susmcgill.ca'>frosh@susmcgill.ca</a>. Frosh is not possible
-            without our partners and we appreciate your support!
-          </h2>
+        <section className="container mx-auto px-4 pt-8 mb-20 space-y-4">
+          <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold">Partners</h1>
+          <div className="flex flex-wrap items-center justify-around leading-normal text-black">
+            <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-8 px-4">
+              <Img fluid={data.alto.childImageSharp.fluid} alt="Alto's" />
+            </div>
+            <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-8 px-4">
+              <Img fluid={data.coc.childImageSharp.fluid} alt="Chef on Call" />
+            </div>
+            <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-8 px-4">
+              <Img fluid={data.diperie.childImageSharp.fluid} alt="La Diperie" />
+            </div>
+            <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-8 px-4">
+              <Img fluid={data.freshii.childImageSharp.fluid} alt="Freshii" />
+            </div>
+            <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-8 px-4">
+              <Img fluid={data.hatter.childImageSharp.fluid} alt="The Mad Hatters" />
+            </div>
+            <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-8 px-4">
+              <Img
+                fluid={data.licm.childImageSharp.fluid}
+                alt="Legal Information Clinic at McGill"
+              />
+            </div>
+            <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-8 px-4">
+              <Img fluid={data.mcgillpizza.childImageSharp.fluid} alt="McGill Pizza" />
+            </div>
+            <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-8 px-4">
+              <Img fluid={data.meddirection.childImageSharp.fluid} alt="Med Direction McGill" />
+            </div>
+            <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-8 px-4">
+              <Img fluid={data.pannizza.childImageSharp.fluid} alt="Pannizza" />
+            </div>
+            <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-8 px-4">
+              <Img fluid={data.wize.childImageSharp.fluid} alt="Wize" />
+            </div>
+          </div>
         </section>
       </Layout>
     )}
   />
-)
+);
