ddf093d7d638126787e10b7392c72ae317b0218f
Kyle Rubenok <kyle@rubenok.ca>
2020-08-25 18:05:14 -0700

    Add Fizz and SPC card

--- src/pages/partners.js
+++ src/pages/partners.js
@@ -120,6 +120,13 @@ export default () => (
             }
           }
         }
+        fizz: file(relativePath: { eq: "sponsors/fizz.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     `}
     render={data => (
@@ -136,57 +143,94 @@ export default () => (
           <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold">Partners</h1>
           <div className="flex flex-wrap items-center justify-around leading-normal text-black">
             <div className="w-full sm:w-full md:w-2/3 lg:w-1/2 my-10 px-6">
-              <Img fluid={data.wize.childImageSharp.fluid} alt="Wize" />
+              <a href="https://wizedemy.com/schools/mcgill">
+                <Img fluid={data.wize.childImageSharp.fluid} alt="Wize" />
+              </a>
             </div>
           </div>
           <div className="flex flex-wrap items-center justify-around leading-normal text-black">
             <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
-              <Img fluid={data.alto.childImageSharp.fluid} alt="Alto's" />
+              <a href="https://altorestaurant.ca/en/">
+                <Img fluid={data.alto.childImageSharp.fluid} alt="Alto's" />
+              </a>
             </div>
             <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
-              <Img fluid={data.apt200.childImageSharp.fluid} alt="Apartment 200" />
+              <a href="https://apt200.com/montreal">
+                <Img fluid={data.apt200.childImageSharp.fluid} alt="Apartment 200" />
+              </a>
             </div>
             <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
-              <Img fluid={data.cibc.childImageSharp.fluid} alt="CIBC" />
+              <a href="https://www.cibc.com/en/personal-banking.html">
+                <Img fluid={data.cibc.childImageSharp.fluid} alt="CIBC" />
+              </a>
             </div>
             <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
-              <Img fluid={data.coc.childImageSharp.fluid} alt="Chef on Call" />
+              <a href="https://www.chefoncalldelivery.com">
+                <Img fluid={data.coc.childImageSharp.fluid} alt="Chef on Call" />
+              </a>
             </div>
             <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
-              <Img fluid={data.diperie.childImageSharp.fluid} alt="La Diperie" />
+              <a href="https://ladiperie.com">
+                <Img fluid={data.diperie.childImageSharp.fluid} alt="La Diperie" />
+              </a>
             </div>
             <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
-              <Img fluid={data.ecoleprivee.childImageSharp.fluid} alt="Ecole Privee" />
+              <a href="https://l.facebook.com/l.php?u=http%3A%2F%2Fclubecoleprivee.com%2F&h=AT2l4yxsAkMeApcdwF4_dG0_rJRztCVxgorxVjbCzeIdb0qDagUvKicGPRFq94d6JJdmmtkCKj6HhNIjSYirxGqoF5mT7tumup9jrHJB72ttmlhWuAyWEkWB9CBCb4emMKLL3LQ">
+                <Img fluid={data.ecoleprivee.childImageSharp.fluid} alt="Ecole Privee" />
+              </a>
             </div>
             <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
-              <Img fluid={data.freshii.childImageSharp.fluid} alt="Freshii" />
+              <a href="https://fizz.ca/en?utm_source=mcgill&utm_medium=&utm_term=referral&utm_content=logo-en&utm_campaign=BTS-partnerships&fbclid=IwAR3Izt79Kb-uWp7d2nLeYMuXIeNnqUkxhsNtpLyIjVvXausJT_Mf0ptmLxs">
+                <Img fluid={data.fizz.childImageSharp.fluid} alt="Fizz" />
+              </a>
             </div>
             <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
-              <Img fluid={data.hatter.childImageSharp.fluid} alt="The Mad Hatters" />
+              <a href="https://www.freshii.com">
+                <Img fluid={data.freshii.childImageSharp.fluid} alt="Freshii" />
+              </a>
             </div>
             <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
-              <Img
-                fluid={data.licm.childImageSharp.fluid}
-                alt="Legal Information Clinic at McGill"
-              />
+              <a href="https://www.madhatterpubmtl.com">
+                <Img fluid={data.hatter.childImageSharp.fluid} alt="The Mad Hatters" />
+              </a>
             </div>
             <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
-              <Img fluid={data.mcgillpizza.childImageSharp.fluid} alt="McGill Pizza" />
+              <a href="https://licm.ca">
+                <Img
+                  fluid={data.licm.childImageSharp.fluid}
+                  alt="Legal Information Clinic at McGill"
+                />
+              </a>
             </div>
             <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
-              <Img fluid={data.meddirection.childImageSharp.fluid} alt="Med Direction McGill" />
+              <a href="https://www.facebook.com/mcgillpizza/">
+                <Img fluid={data.mcgillpizza.childImageSharp.fluid} alt="McGill Pizza" />
+              </a>
             </div>
             <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
-              <Img fluid={data.oxford.childImageSharp.fluid} alt="Oxford" />
+              <a href="https://md.sus.mcgill.ca">
+                <Img fluid={data.meddirection.childImageSharp.fluid} alt="Med Direction McGill" />
+              </a>
             </div>
             <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
-              <Img fluid={data.pannizza.childImageSharp.fluid} alt="Pannizza" />
+              <a href="https://www.oxfordproperties.com/corp">
+                <Img fluid={data.oxford.childImageSharp.fluid} alt="Oxford" />
+              </a>
             </div>
-            {/* <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
-              <Img fluid={data.spc.childImageSharp.fluid} alt="SPC" />
-            </div> */}
             <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
-              <Img fluid={data.spin.childImageSharp.fluid} alt="Spin Energie" />
+              <a href="http://www.pannizza.com">
+                <Img fluid={data.pannizza.childImageSharp.fluid} alt="Pannizza" />
+              </a>
+            </div>
+            <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
+              <a href="https://spccard.ca">
+                <Img fluid={data.spc.childImageSharp.fluid} alt="SPC" />
+              </a>
+            </div>
+            <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 my-10 px-6">
+              <a href="https://www.spinenergie.com/en">
+                <Img fluid={data.spin.childImageSharp.fluid} alt="Spin Energie" />
+              </a>
             </div>
           </div>
         </section>
